df133b6dd1ed302595259ae473b10c99e1faa265
abhishek71994 <abhishek71994@gmail.com>
2017-12-31 23:15:59 +0530

    Final refactoring

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express'),
 bodyParser  = require('body-parser'),
 methodOverride = require('method-override'),
+expressSanitizer = require('express-sanitizer'),
 mongoose	= require('mongoose'),
 	app 	= express();
 
@@ -9,6 +10,7 @@ mongoose.connect("mongodb://localhost/blog");
 app.set("view engine","ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(expressSanitizer());//the only requirement is that it goes after the bodyParser
 app.use(methodOverride("_method"));
 
 //database configuration
@@ -43,6 +45,7 @@ app.get("/blogs/new",function(req,res){
 //create route
 app.post("/blogs",function(req,res){
 	//create blog
+	req.body.blog.body = req.sanitize(req.body.blog.body);
 	Blog.create(req.body.blog, function(err,newBlog){
 		if(err){
 			res.render("new");
@@ -100,7 +103,7 @@ app.put("/blogs/:id",function(req,res){
 	// 		res.render("edit",{blog:oneBlog});
 	// 	}
 	// });
-
+	req.body.blog.body = req.sanitize(req.body.blog.body);
 	//new type
 	Blog.findByIdAndUpdate(req.params.id,req.body.blog,function(err,updatedBlog){
 			if(err){
